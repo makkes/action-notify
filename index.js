369bf9769009d2e334c56fc95e54cc2e4b27e3b8
Max Jonas Werner <mwerner@mesosphere.io>
2019-09-20 16:51:00 +0200

    Playing around

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const slack = require('./slack.js')
 
+messages = {
+    'issue': issueAction
+}
+
+function issueAction(payload) {
+    return `Issue <${payload.issue.html_url}|${payload.issue.title}> has been ${payload.action}`
+}
+
 try {
+    console.log('Context: ', github.context)
     const payload = github.context.payload
-    slack.sendMessage(core.getInput('slack-url'), `Issue <${payload.issue.html_url}|${payload.issue.title}> has been ${payload.action}`, err => {
+    slack.sendMessage(core.getInput('slack-url'), 'test', err => {
         if (typeof err !== 'undefined') {
             core.setFailed(err)
         }
