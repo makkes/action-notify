cd9abb8c6b1bcb650cd28fe39557df8f4c8c5bc3
Max Jonas Werner <mwerner@mesosphere.io>
2019-09-20 16:30:35 +0200

    Debug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const slack = require('./slack.js')
 
 try {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`Payload: ${payload['issue']}`)
+    console.log('Payload: ', typeof payload)
     slack.sendMessage(core.getInput('slack-url'), `Issue <'${payload.issue.html_url}'|'"${payload.issue.title}"'> has been '${payload.action}'`, err => {
         core.setFailed(err)
     })
